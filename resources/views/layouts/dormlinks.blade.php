681489059907bed7640ea34a27bab602837861ed
hasmin12 <idatuhasmin@gmail.com>
2024-01-18 22:12:04 +0800

    links and style asset

--- resources/views/layouts/dormlinks.blade.php
+++ resources/views/layouts/dormlinks.blade.php
@@ -1,23 +1,23 @@
  <!-- JavaScript Libraries -->
  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
- <script src="/lib/chart/chart.min.js"></script>
- <script src="/lib/easing/easing.min.js"></script>
- <script src="/lib/waypoints/waypoints.min.js"></script>
- <script src="/lib/owlcarousel/owl.carousel.min.js"></script>
- <script src="/lib/tempusdominus/js/moment.min.js"></script>
- <script src="/lib/tempusdominus/js/moment-timezone.min.js"></script>
- <script src="/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+ <script src="{{ asset('lib/chart/chart.min.js') }}"></script>
+ <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+ <script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+ <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+ <script src="{{ asset('lib/tempusdominus/js/moment.min.js') }}"></script>
+ <script src="{{ asset('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
+ <script src="{{ asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js" integrity="sha512-iusSCweltSRVrjOz+4nxOL9OXh2UA0m8KdjsX8/KUUiJz+TCNzalwE0WE6dYTfHDkXuGuHq3W9YIhDLN7UNB0w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
  <!-- Template Javascript -->
- <script src="/js/main.js"></script>
- <script src="/js/auth.js"></script>
- <script src="/js/resident.js"></script>
- <script src="/js/laundry.js"></script>
- <script src="/js/room.js"></script>
- <script src="/js/announcement.js"></script>
- <script src="/js/lostandfound.js"></script>
- <script src="/js/beds.js"></script>
+ <script src="{{ asset('js/main.js') }}"></script>
+ <script src="{{ asset('js/auth.js') }}"></script>
+ <script src="{{ asset('js/resident.js') }}"></script>
+ <script src="{{ asset('js/laundry.js') }}"></script>
+ <script src="{{ asset('js/room.js') }}"></script>
+ <script src="{{ asset('js/announcement.js') }}"></script>
+ <script src="{{ asset('js/lostandfound.js') }}"></script>
+ <script src="{{ asset('js/beds.js') }}"></script>
 
 
