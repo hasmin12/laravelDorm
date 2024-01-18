b1c063f0aec12ba195dcfbb7d8eb3b065512ea52
hasmin12 <idatuhasmin@gmail.com>
2024-01-18 22:00:14 +0800

    links

--- resources/views/layouts/dormlinks.blade.php
+++ resources/views/layouts/dormlinks.blade.php
@@ -2,22 +2,22 @@
  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
  <script src="/lib/chart/chart.min.js"></script>
- <script src="lib/easing/easing.min.js"></script>
- <script src="lib/waypoints/waypoints.min.js"></script>
- <script src="lib/owlcarousel/owl.carousel.min.js"></script>
- <script src="lib/tempusdominus/js/moment.min.js"></script>
- <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
- <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+ <script src="/lib/easing/easing.min.js"></script>
+ <script src="/lib/waypoints/waypoints.min.js"></script>
+ <script src="/lib/owlcarousel/owl.carousel.min.js"></script>
+ <script src="/lib/tempusdominus/js/moment.min.js"></script>
+ <script src="/lib/tempusdominus/js/moment-timezone.min.js"></script>
+ <script src="/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js" integrity="sha512-iusSCweltSRVrjOz+4nxOL9OXh2UA0m8KdjsX8/KUUiJz+TCNzalwE0WE6dYTfHDkXuGuHq3W9YIhDLN7UNB0w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
  <!-- Template Javascript -->
- <script src="js/main.js"></script>
- <script src="js/auth.js"></script>
- <script src="js/resident.js"></script>
- <script src="js/laundry.js"></script>
- <script src="js/room.js"></script>
- <script src="js/announcement.js"></script>
- <script src="js/lostandfound.js"></script>
- <script src="js/beds.js"></script>
+ <script src="/js/main.js"></script>
+ <script src="/js/auth.js"></script>
+ <script src="/js/resident.js"></script>
+ <script src="/js/laundry.js"></script>
+ <script src="/js/room.js"></script>
+ <script src="/js/announcement.js"></script>
+ <script src="/js/lostandfound.js"></script>
+ <script src="/js/beds.js"></script>
 
 
