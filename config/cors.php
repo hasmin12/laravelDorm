0c8acd02fc67bfdd25a389d16548c495dd830e80
hasmin12 <idatuhasmin@gmail.com>
2024-01-18 22:27:06 +0800

    Kernel

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://laraveldorm-production.up.railway.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
