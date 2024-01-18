e17d35c58d8d9aa9e1d2755e43a997512c38a970
hasmin12 <idatuhasmin@gmail.com>
2024-01-18 22:22:42 +0800

    cors2

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['https://laraveldorm-production.up.railway.app'],
+    'allowed_origins' => ['http://laraveldorm-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
