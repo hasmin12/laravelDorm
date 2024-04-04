665b764a7f128aa7d267da9985d89700addb8689
hasmin12 <idatuhasmin@gmail.com>
2024-04-05 01:26:07 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-    // 'allowed_origins' => ['https://laraveldorm-production.up.railway.app'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://dormxtend-dormxtend.bdsahf.easypanel.host'],
 
     'allowed_origins_patterns' => [],
 
