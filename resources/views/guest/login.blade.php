ad3be62796aa99fa6643a92b46d9d9cab74dd591
maye <hrvygllrd>
2024-05-13 22:47:59 +0800

    login pic

--- resources/views/guest/login.blade.php
+++ resources/views/guest/login.blade.php
@@ -98,7 +98,7 @@
 <!-- Navbar End -->
 
 <div class="full-width-container">
-    <section class="hero-section" style="background-image: url('{{ secure_asset('/img/tt.jpg')}}')">
+    <section class="hero-section" style="background-image: url('{{ secure_asset('/img/bsad.png')}}')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">                 
