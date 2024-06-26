67c00eaa725bd8b72298e9f4c88506153ea7b103
hasmin12 <idatuhasmin@gmail.com>
2024-06-26 10:27:21 +0800

    reports

--- public/js/admin/dorm/reports.js
+++ public/js/admin/dorm/reports.js
@@ -23,9 +23,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function changeDiv() {
     const reportValue = document.getElementById('report').value;
+    const selectedBranch = document.querySelector('input[name="branchbtnradio"]:checked').value;
 
     // Get the div elements
     const residentDiv = document.getElementById('resident-reports');
+    const hosteldiv = document.getElementById('hostel-reports');
+
     const maintenanceDiv = document.getElementById('maintenance-reports');
     const visitorDiv = document.getElementById('visitors-reports');
     const laundryDiv = document.getElementById('laundry-reports');
@@ -33,11 +36,23 @@ function changeDiv() {
     const branchDiv = document.getElementById('branchDiv');
 
     if (reportValue === "Residents") {
-        residentDiv.style.display = 'block';
-        branchDiv.style.display = 'block';
-        maintenanceDiv.style.display = 'none';
-        visitorDiv.style.display = 'none';
-        laundryDiv.style.display = 'none';
+        if(selectedBranch  === "Dormitory"){
+            residentDiv.style.display = 'block';
+            branchDiv.style.display = 'block';
+            maintenanceDiv.style.display = 'none';
+            visitorDiv.style.display = 'none';
+            laundryDiv.style.display = 'none';
+            hosteldiv.style.display = 'none';
+
+        }else{
+            residentDiv.style.display = 'none';
+            branchDiv.style.display = 'block';
+            maintenanceDiv.style.display = 'none';
+            visitorDiv.style.display = 'none';
+            laundryDiv.style.display = 'none';
+            hosteldiv.style.display = 'block';
+        }
+        
 
     } else if (reportValue === "Maintenance") {
         residentDiv.style.display = 'none';
@@ -45,6 +60,8 @@ function changeDiv() {
         maintenanceDiv.style.display = 'block';
         visitorDiv.style.display = 'none';
         laundryDiv.style.display = 'none';
+        hosteldiv.style.display = 'none';
+
 
     } else if (reportValue === "Visitors") {
         residentDiv.style.display = 'none';
@@ -52,6 +69,7 @@ function changeDiv() {
         maintenanceDiv.style.display = 'none';
         visitorDiv.style.display = 'block';
         laundryDiv.style.display = 'none';
+        hosteldiv.style.display = 'none';
 
     }else{
         residentDiv.style.display = 'none';
@@ -59,6 +77,8 @@ function changeDiv() {
         maintenanceDiv.style.display = 'none';
         visitorDiv.style.display = 'none';
         laundryDiv.style.display = 'block';
+        hosteldiv.style.display = 'none';
+
     }
 }
 function fetchResidentsReport() {
@@ -84,8 +104,9 @@ function fetchResidentsReport() {
         return response.json();
     })
     .then(data => {
-        const residentTableBody = document.querySelector('#residentReportBody');
-        residentTableBody.innerHTML = '';
+        if (selectedBranch=="Dormitory"){
+            const residentTableBody = document.querySelector('#residentReportBody');
+            residentTableBody.innerHTML = '';
 
             data.forEach(resident => {
                 const createdAtDate = new Date(resident.created_at);
@@ -101,6 +122,32 @@ function fetchResidentsReport() {
                 `;
                 residentTableBody.innerHTML += row;
             });
+        }else{
+            const hostelReportBody = document.querySelector('#hostelReportBody');
+            residentTableBody.innerHTML = '';
+
+            data.forEach(reservation => {
+                const reservation_date = new Date(reservation.reservation_date);
+                const checkin_date = new Date(reservation.checkin_date);
+                const checkout_date = new Date(reservation.checkout_date);
+
+                const formatResercation = `${reservation_date.getMonth() + 1}/${reservation_date.getDate()}/${reservation_date.getFullYear()}`;
+                const formatCheckin = `${checkin_date.getMonth() + 1}/${checkin_date.getDate()}/${checkin_date.getFullYear()}`;
+                const formatCheckout = `${checkout_date.getMonth() + 1}/${checkout_date.getDate()}/${checkout_date.getFullYear()}`;
+
+                const row = `
+                        <td>${reservation.name}</td>
+                        <td>${reservation.roomName}</td>
+                        <td>${reservation.totalPayment}</td>
+                        <td>${formatResercation}</td>
+                        <td>${formatCheckin}</td>
+                        <td>${formatCheckout }</td>
+                        <td>${reservation.status}</td>
+                `;
+                hostelReportBody.innerHTML += row;
+
+            });
+        }
     
     })
     .catch(error => {
@@ -328,7 +375,40 @@ function downloadResidentsReport() {
             const a = document.createElement('a');
             a.style.display = 'none';
             a.href = url;
-            a.download = 'residents_report.pdf';
+            a.download = 'Maintenance_report.pdf';
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(error => {
+            console.error('Error fetching residents report:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'An error occurred while fetching the residents report. Please try again.',
+            });
+        });
+
+    } else if (reportValue === "Visitors") {
+        fetch(`/api/generateVisitorsReport?branch=${selectedBranch}&change=${changeValue}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.blob(); // Get the response as a Blob (binary data)
+        })
+        .then(blob => {
+            // Create a link element, use it to download the PDF
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = 'visitors_report.pdf';
             document.body.appendChild(a);
             a.click();
             window.URL.revokeObjectURL(url);
@@ -343,7 +423,7 @@ function downloadResidentsReport() {
         });
 
     }else{
-        fetch(`/api/generateVisitorsReport?change=${changeValue}`, {
+        fetch(`/api/generateLaundryReport?change=${changeValue}`, {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + token,
@@ -360,7 +440,7 @@ function downloadResidentsReport() {
             const a = document.createElement('a');
             a.style.display = 'none';
             a.href = url;
-            a.download = 'residents_report.pdf';
+            a.download = 'laundry_schedule_report.pdf';
             document.body.appendChild(a);
             a.click();
             window.URL.revokeObjectURL(url);
--- resources/views/admin/dorm/reports.blade.php
+++ resources/views/admin/dorm/reports.blade.php
@@ -1,166 +1,207 @@
 @extends('layouts.base')
 @section('content')
-<div class="container-xxl position-relative bg-white d-flex p-0">
-<!-- Spinner Start -->
-<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-        <span class="sr-only">Loading...</span>
-    </div>
-</div>
-<!-- Spinner End -->
-
-@include('layouts.sidebar.dorm.admin')
-
-<!-- Content Start -->
-<div class="content">
-    @include('layouts.navbar')
-    <div class="bg-light rounded p-4">
-        <div class="d-flex align-items-center justify-content-between mb-2">
-            <h3 class="mb-0">Reports</h3>
-        </div>
-        <div class="row mb-2">
-            <select id="report" class="form-control" name="report">
-                <option value="Residents">Residents</option>
-                <option value="Maintenance">Maintenance</option>
-                <option value="Visitors">Visitors</option>
-                <option value="Laundry">Laundry Schedules</option>
-
-                {{-- <option value="Announcements">Announcements</option> --}}
-
-
-            </select>
-        </div>
-            <div id="branchDiv" class="btn-group" role="group">
-                <input type="radio" class="btn-check" name="branchbtnradio" id="branchbtnradio1" autocomplete="off" checked value="Dormitory">
-                <label class="btn btn-outline-primary" for="branchbtnradio1">Dormitory</label>
-
-                <input type="radio" class="btn-check" name="branchbtnradio" id="branchbtnradio2" autocomplete="off" value="Hostel">
-                <label class="btn btn-outline-primary" for="branchbtnradio2">Hostel</label>
-            </div>
-            <div class="btn-group" role="group">
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked value="All">
-                <label class="btn btn-outline-primary" for="btnradio1">All</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" value="Daily">
-                <label class="btn btn-outline-primary" for="btnradio2">Daily</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" value="Weekly">
-                <label class="btn btn-outline-primary" for="btnradio3">Weekly</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off" value="Monthly">
-                <label class="btn btn-outline-primary" for="btnradio4">Monthly</label>
-
-                <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off" value="Yearly">
-                <label class="btn btn-outline-primary" for="btnradio5">Yearly</label>
-            </div>
-            <button id="downloadPdfBtn" class="btn btn-primary">Download PDF</button>
-        </div>
-        <div id="resident-reports">
-            <div class="bg-light text-center rounded p-4">
-                <div class="d-flex align-items-center justify-content-between mb-4">
-                    <h6 class="mb-0">Residents Information Report</h6>
-                </div>
-                <div class="table-responsive">
-                    <table class="table text-start align-middle table-bordered table-hover mb-0" class="table table-dark">
-                        <thead>
-                            <tr class="text-dark">
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Type</th>
-                                <th>Birth Date</th>
-                                <th>Sex</th>
-                                <th>Phone Number</th>
-                                <th>Registered</th>
-                            </tr>
-                        </thead>
-                        <tbody id="residentReportBody">
-                            
-                        </tbody>
-                    </table>
-                </div>
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
             </div>
         </div>
+        <!-- Spinner End -->
 
-        
-        
-        <div id="maintenance-reports">
-            <div class="bg-light text-center rounded p-4">
-                <div class="d-flex align-items-center justify-content-between mb-4">
-                    <h6 class="mb-0">Maintenance Report</h6>
+        @include('layouts.sidebar.dorm.admin')
+
+        <!-- Content Start -->
+        <div class="content">
+            @include('layouts.navbar')
+            <div class="bg-light rounded p-4">
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                    <h3 class="mb-0">Reports</h3>
                 </div>
-                <div class="table-responsive">
-                    <table class="table text-start align-middle table-bordered table-hover mb-0" class="table table-dark">
-                        <thead>
-                            <tr class="text-dark">
-                                <th>Resident Name</th>
-                                <th>Room Number</th>
-                                <th>Technician</th>
-                                <th>Maintenance Type</th>
-                                <th>Date Requested</th>
-                                <th>Date Finished</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody id="maintenanceReportBody">
-                            
-                        </tbody>
-                    </table>
+                <div class="row mb-2">
+                    <select id="report" class="form-control" name="report">
+                        <option value="Residents">Residents</option>
+                        <option value="Maintenance">Maintenance</option>
+                        <option value="Visitors">Visitors</option>
+                        <option value="Laundry">Laundry Schedules</option>
+
+                        {{-- <option value="Announcements">Announcements</option> --}}
+
+
+                    </select>
+                </div>
+                <div id="branchDiv" class="btn-group" role="group">
+                    <input type="radio" class="btn-check" name="branchbtnradio" id="branchbtnradio1" autocomplete="off"
+                        checked value="Dormitory">
+                    <label class="btn btn-outline-primary" for="branchbtnradio1">Dormitory</label>
+
+                    <input type="radio" class="btn-check" name="branchbtnradio" id="branchbtnradio2" autocomplete="off"
+                        value="Hostel">
+                    <label class="btn btn-outline-primary" for="branchbtnradio2">Hostel</label>
+                </div>
+                <div class="btn-group" role="group">
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked
+                        value="All">
+                    <label class="btn btn-outline-primary" for="btnradio1">All</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off"
+                        value="Daily">
+                    <label class="btn btn-outline-primary" for="btnradio2">Daily</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off"
+                        value="Weekly">
+                    <label class="btn btn-outline-primary" for="btnradio3">Weekly</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off"
+                        value="Monthly">
+                    <label class="btn btn-outline-primary" for="btnradio4">Monthly</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off"
+                        value="Yearly">
+                    <label class="btn btn-outline-primary" for="btnradio5">Yearly</label>
+                </div>
+                <button id="downloadPdfBtn" class="btn btn-primary">Download PDF</button>
+            </div>
+            <div id="resident-reports">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Residents Information Report</h6>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0"
+                            class="table table-dark">
+                            <thead>
+                                <tr class="text-dark">
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Type</th>
+                                    <th>Birth Date</th>
+                                    <th>Sex</th>
+                                    <th>Phone Number</th>
+                                    <th>Registered</th>
+                                </tr>
+                            </thead>
+                            <tbody id="residentReportBody">
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-      
-        <div id="visitors-reports">
-            <div class="bg-light text-center rounded p-4">
-                <div class="d-flex align-items-center justify-content-between mb-4">
-                    <h6 class="mb-0">Visitors Report</h6>
-                </div>
-                <div class="table-responsive">
-                    <table class="table text-start align-middle table-bordered table-hover mb-0" class="table table-dark">
-                        <thead>
-                            <tr class="text-dark">
-                                <th>Visitor Name</th>
-                                <th>Contact Number</th>
-                                <th>Visit Date</th>
-                                <th>Resident Name</th>
-                                <th>Relationship</th>
-                                <th>Purpose</th>
-                            </tr>
-                        </thead>
-                        <tbody id="visitorReportBody">
-                            
-                        </tbody>
-                    </table>
+
+
+            <div id="hostel-reports">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Hostel Reservations Report</h6>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0"
+                            class="table table-dark">
+                            <thead>
+                                <tr class="text-dark">
+                                    <th>Name</th>
+                                    <th>Room</th>
+                                    <th>Total Payment</th>
+                                    <th>Reservation Date</th>
+                                    <th>Check-In Date</th>
+                                    <th>Check-Out Dater</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody id="hostelReportBody">
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div id="laundry-reports">
-            <div class="bg-light text-center rounded p-4">
-                <div class="d-flex align-items-center justify-content-between mb-4">
-                    <h6 class="mb-0">Laundry Schedule Report</h6>
-                </div>
-                <div class="table-responsive">
-                    <table class="table text-start align-middle table-bordered table-hover mb-0" class="table table-dark">
-                        <thead>
-                            <tr class="text-dark">
-                                <th>Resident Name</th>
 
-                                <th>Laundry Date</th>
-                                <th>Laundry Time</th>
-                                <th>Status</th>
-                                <th>Created At</th>
 
-                            </tr>
-                        </thead>
-                        <tbody id="laundryReportBody">
-                            
-                        </tbody>
-                    </table>
+
+            <div id="maintenance-reports">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Maintenance Report</h6>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0"
+                            class="table table-dark">
+                            <thead>
+                                <tr class="text-dark">
+                                    <th>Resident Name</th>
+                                    <th>Room Number</th>
+                                    <th>Technician</th>
+                                    <th>Maintenance Type</th>
+                                    <th>Date Requested</th>
+                                    <th>Date Finished</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody id="maintenanceReportBody">
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-      
-        {{-- <div id="announcements-reports">
+
+            <div id="visitors-reports">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Visitors Report</h6>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0"
+                            class="table table-dark">
+                            <thead>
+                                <tr class="text-dark">
+                                    <th>Visitor Name</th>
+                                    <th>Contact Number</th>
+                                    <th>Visit Date</th>
+                                    <th>Resident Name</th>
+                                    <th>Relationship</th>
+                                    <th>Purpose</th>
+                                </tr>
+                            </thead>
+                            <tbody id="visitorReportBody">
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div id="laundry-reports">
+                <div class="bg-light text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Laundry Schedule Report</h6>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0"
+                            class="table table-dark">
+                            <thead>
+                                <tr class="text-dark">
+                                    <th>Resident Name</th>
+
+                                    <th>Laundry Date</th>
+                                    <th>Laundry Time</th>
+                                    <th>Status</th>
+                                    <th>Created At</th>
+
+                                </tr>
+                            </thead>
+                            <tbody id="laundryReportBody">
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            {{-- <div id="announcements-reports">
             <div class="bg-light text-center rounded p-4">
                 <div class="d-flex align-items-center justify-content-between mb-4">
                     <h6 class="mb-0">Announcements Report</h6>
@@ -185,14 +226,13 @@
                 </div>
             </div>
         </div> --}}
+        </div>
     </div>
-</div>
-<!-- Content End -->
+    <!-- Content End -->
 
 
-<!-- Back to Top -->
-<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-</div>
-<script src="{{ secure_asset('js/admin/dorm/reports.js') }}"></script>
-
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+    <script src="{{ secure_asset('js/admin/dorm/reports.js') }}"></script>
 @endsection
