3cf4aeed237e84a81073f2df5bcdf9919afe6f1b
hasmin12 <idatuhasmin@gmail.com>
2024-09-01 23:20:54 +0800

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -84,6 +84,7 @@ class AuthController extends Controller
 
     public function signout(Request $request)
     {
+        $request->user()->currentAccessToken()->delete();
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         return response()->json(['message' => 'Successfully logged out']);
